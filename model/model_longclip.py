4601e0b46183a5b96dc5f4d5083df6b2d98887f1
aidanbell073@gmail.com <aidanbell073@gmail.com>
2025-04-11 14:04:47 -0400

    small optimizations, and removal of label_smoothing

--- model/model_longclip.py
+++ model/model_longclip.py
@@ -446,18 +446,15 @@ class CLIP(nn.Module):
         image_features = image_features / image_features.norm(dim=1, keepdim=True)
         text_features = text_features / text_features.norm(dim=1, keepdim=True)
         
-        sim_i2t = torch.matmul(image_features, text_features.T)
-        sim_t2i = sim_i2t.T
-        
-        sim_i2t = self.logit_scale.exp() * sim_i2t
-        sim_t2i = self.logit_scale.exp() * sim_t2i
+        sim_i2t = self.logit_scale.exp() * torch.matmul(image_features, text_features.t())
+        sim_t2i = sim_i2t.t()
 
         bs = images.size(0)
         targets = torch.arange(0, bs, dtype=torch.long, device=images.device)
         
         loss = (
-                F.cross_entropy(sim_i2t, targets, label_smoothing=0.1)
-                + F.cross_entropy(sim_t2i, targets, label_smoothing=0.1)
+                F.cross_entropy(sim_i2t, targets)
+                + F.cross_entropy(sim_t2i, targets)
             ) / 2
         
         return loss
@@ -473,18 +470,15 @@ class CLIP(nn.Module):
         all_image_features = torch.cat(torch.distributed.nn.all_gather(image_features), dim=0) # Gathers with grad, effectively simulating a larger batch size for contrastive loss
         all_text_features = torch.cat(torch.distributed.nn.all_gather(text_features), dim=0)
         
-        sim_i2t = torch.matmul(image_features, all_text_features.T)
-        sim_t2i = torch.matmul(text_features, all_image_features.T)
-        
-        sim_i2t = self.logit_scale.exp() * sim_i2t
-        sim_t2i = self.logit_scale.exp() * sim_t2i
+        sim_i2t = self.logit_scale.exp() * torch.matmul(image_features, all_text_features.t())
+        sim_t2i = self.logit_scale.exp() * torch.matmul(text_features, all_image_features.t())
 
         bs = images.size(0)
         targets = torch.arange(rank * bs, (rank + 1) * bs, dtype=torch.long, device=images.device)
         
         loss = (
-                F.cross_entropy(sim_i2t, targets, label_smoothing=0.1)
-                + F.cross_entropy(sim_t2i, targets, label_smoothing=0.1)
+                F.cross_entropy(sim_i2t, targets)
+                + F.cross_entropy(sim_t2i, targets)
             ) / 2
         
         return loss
